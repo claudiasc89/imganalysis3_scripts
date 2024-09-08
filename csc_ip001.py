0cdd6c3a4095eb4d51158a758f91329af8890411
claudiasc89 <127869552+claudiasc89@users.noreply.github.com>
2024-09-08 21:48:24 +0200

    csc_ip0001.py udpated

--- csc_ip001.py
+++ csc_ip001.py
@@ -69,7 +69,7 @@ import nd2
 import os
 import numpy as np
 from tifffile import imsave, imread
-import pandas as pd
+
 
 # User-configurable parameters
 # ============================
@@ -100,7 +100,25 @@ dimensions_type= ('t','z','x','y') #t for time, z for z_stack, x and y dimension
 
 def filenaming(folder,file):
     '''
-    
+    Generates file paths and names based on the given folder, file, and projection type.
+
+    Parameters
+    ----------
+    folder : str
+        The directory path where the file is located.
+    file : str
+        The name of the file, including its extension.
+    proj_type : str
+        The type of projection to include in the saved file name.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the following:
+        - filepath (str): The full path to the file.
+        - filename (str): The base name of the file.
+        - savename (str): The name to use when saving the file, including the projection type.
+        - basename (str): The base name of the file without the extension.
     '''
     
     filepath = os.path.join(folder, file)
@@ -111,13 +129,34 @@ def filenaming(folder,file):
     names=(filepath,filename,savename,basename)
     return names
 
-def focused_z(image):
-    "This function determines the most focused z slice for fluorescent channels within a multi-dimensional array"
-    'to do so it calculates the frame with the highest standard deviation'
-    sd=image.std(axis=(1,2)) #by axis we are telling it to flatten the first and second dimensions, so Y and X and get the sd of all the values
+def focused_z(image, dimensions_indices):
+    '''
+    Determines the most focused z-slice in a multi-dimensional image array by calculating the standard deviation
+    of each slice along the z-axis (depth) and identifying the slice with the highest standard deviation.
+
+    Parameters
+    ----------
+    image : numpy.ndarray
+        A 3D array representing an image stack. 
+
+    dimensions_indices : dict
+        A dictionary mapping dimension names (e.g., 'x', 'y', 'z') to their corresponding axis indices in the `image` array.
+        This allows the function to correctly identify which axes correspond to the spatial dimensions (X and Y) 
+        and the depth dimension (Z).
+
+    Returns
+    -------
+    tuple
+        A tuple containing:
+        - sd (numpy.ndarray): An array of standard deviation values for each z-slice, with the same length as the number 
+          of slices along the z-axis.
+        - maxsd (int): The index of the z-slice with the highest standard deviation, indicating the most focused slice.
+    '''
+    axis=(dimensions_indices['x'],dimensions_indices['y'])
+    sd=image.std(axis=axis) #by axis we are telling it to flatten the first and second dimensions, so Y and X and get the sd of all the values
     maxsd=sd.argmax() 
     return sd, maxsd
-
+'''
 def focused_z_v2(image):
     'This function determines the most focused z slice within a multi-stack brightfield image'
     'to do so it calculates the variance between the sd deviation of consecutive images'
@@ -130,9 +169,30 @@ def focused_z_v2(image):
     sd_df=pd.DataFrame(sd_dict)
     minsddiff=diff_sd.argmin() #we have to substract one to correclty guess the z-stack where the image is best focused
     return sd_df, minsddiff
+'''
 
 def import_image(filepath):
-    'This function checks wheter the image is a TIFF file or an ND2 and uses the appropiate code to import it'
+    '''
+    Imports an image from a file, supporting TIFF and ND2 formats. 
+
+    Depending on the file extension, the function selects the appropriate method to read the image.
+
+    Parameters
+    ----------
+    filepath : str
+        The full path to the image file that needs to be imported. 
+        
+    Returns
+    -------
+    img : numpy.ndarray
+        The imported image as a multi-dimensional NumPy array.
+
+    Raises
+    ------
+    ValueError
+        If the file format is not supported (neither TIFF nor ND2).
+    '''
+    # Determine file extension and import image accordingly
     if filename.endswith('.tif'):
         img = imread(filepath)  # Import TIFF file
     elif filename.endswith('.nd2'):
@@ -141,20 +201,33 @@ def import_image(filepath):
         raise ValueError(f"Unsupported file type: {filename}")
     return img
 
-def projection_3D (img):
-    'This function performs the desired projection on the image'
+def projection_3D (img, dimensions_indices):
+    '''
+    Performs a specified projection on a 3D image stack.
+
+    This function supports "best", "mean", and "max" projection types. For the "best" type, it selects the z-slice with the highest 
+    standard deviation. For "mean" and "max" types, it calculates a projection based on a defined stack depth around the most focused 
+    z-slice.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        A 3D image stack to be projected.
+
+    Returns
+    -------
+    None
+        The function saves the projected image and logs the projection details in a text file.
+    '''    
     global proj_type
     global maxsd
     global filename
-    global dirpath
     global stack_step
     global dimensions
     global dimensions_map
-    global dimensions_indices
     
     # Get variables from dimensions_map
     z = dimensions_map['z']
-    
     
     # Get the two first blocks of filename 
     parts = filename.split('_')
@@ -197,10 +270,15 @@ def projection_3D (img):
                 # in this case the best focused plane is central so we simply sue start_z and stop_z
                 z_range = range(start_z,stop_z) #we add one so it the stop stack is included
          
-            # Log the z-stack used
-            with open(log_file, 'a') as f:
-             log_message = f"Image: {filename}, z-stack range used: {z_range}\n"
-             f.write(log_message)
+            # Log the z-stack used (we use different approaches for time-lapses)
+            if 't' in dimensions_map:
+                with open(log_file, 'a') as f:
+                 log_message = f"Image: {filename},timepoint {t}, z-stack range used: {z_range}\n"
+                 f.write(log_message)
+            else:
+                with open(log_file, 'a') as f:
+                 log_message = f"Image: {filename}, z-stack range used: {z_range}\n"
+                 f.write(log_message)
     
         # to slice the image
         img_z = img.take(z_range, axis=dimensions_indices['z']) # find the axis where z is stored
@@ -214,85 +292,7 @@ def projection_3D (img):
             # use numpy to perform the mean projeaction
             img_proj=np.max(img_z,axis=dimensions_indices['z']) #I perform mean projection
             # Max does not require dtype because it returns the maximum value directly, maintaining the same data type as the input.
-
-        # Save the projected image
-        img_path = os.path.join(dirpath, f"{output_name}_{proj_type}.tif")
-        imsave(img_path,img_proj)        
-    
-    return None
-
-def projection_4D (img,subfolder):
-    'This function performs the desired projection on the image'
-    global proj_type
-    global maxsd
-    global filename
-    global stack_step
-    global dimensions
-    global dimensions_map
-    global dimensions_indices
-    
-    # Get variables from dimensions_map
-    z = s_dimensions_map['z']
-    
-    # Get the two first blocks of filename 
-    parts = filename.split('_')
-    output_name = '_'.join(parts[:2])
-    
-    # Create a log file to store the information about the z-stack used
-    log_file = os.path.join(folder,subfolder, f"{output_name}_{proj_type}_log.txt")
-    
-    if proj_type == 'best':
-        img_proj = img[maxsd,:,:]
         
-        # Log the z-stack used
-        with open(log_file, 'a') as f:
-            log_message = f"Image: {filename}, z-stack used: {maxsd}\n"
-            f.write(log_message)
-    
-    elif proj_type == 'mean' or proj_type == 'max':
-        # Calculate stack depth
-        stack_depth = stack_step*2 + 1 # we duplicate the step to include stacks below and above and add 1 to include the central stack
-        
-        # Check that stack_depth is smaller or equal than image dimensions
-        if stack_depth > s_dimensions_map['z']:
-            raise ValueError(f"For image {filename} stack depth should be smaller or equal than image z dimensions")
-        
-        else:
-            # To calculate the first and last stack included in the projection according to the best focused
-            start_z = maxsd-stack_step
-            stop_z = maxsd+stack_step+1 # add 1 to make sure that the stop_z is included
-            
-            # it might be than the best focused is not central so there are not enough z-stacks to accomodate our will
-            # to check if best focused is closer to the first stack
-            if start_z<0:
-                # define the z_range that will be used for the projection, in this case we use the first z-stacks
-                z_range = range(0,stack_depth)
-            # to check if best focused is closer to the last stack
-            elif stop_z > z:
-                # in this case we use the last z-stacks
-                z_range = range(z-stack_depth,z)
-            else:
-                # in this case the best focused plane is central so we simply sue start_z and stop_z
-                z_range = range(start_z,stop_z) #we add one so it the stop stack is included
-         
-            # Log the z-stack used
-            with open(log_file, 'a') as f:
-             log_message = f"Image: {filename},timepoint {t}, z-stack range used: {z_range}\n"
-             f.write(log_message)
-    
-        # to slice the image
-        img_z = img.take(z_range, axis=s_dimensions_indices['z']) # find the axis where z is stored
-        
-        # Perform the appropiate projection
-        
-        if proj_type =='mean':
-            # use numpy to perform the mean projeaction
-            img_proj=np.mean(img_z,axis=s_dimensions_indices['z'], dtype=np.uint16) #I perform mean projection
-        elif proj_type =='max':
-            # use numpy to perform the mean projeaction
-            img_proj=np.max(img_z,axis=s_dimensions_indices['z']) #I perform mean projection
-            # Max does not require dtype because it returns the maximum value directly, maintaining the same data type as the input.
-
     return img_proj
 
 def get_dimensions(image):
@@ -345,8 +345,6 @@ def make_folder (*folder_names):
             
             # Create the folder if it doesn't exist
             os.makedirs(folder_path)
-            
-            
             
 def redefine_dimensions(dimension_to_remove):
     '''
@@ -402,10 +400,14 @@ for file in sorted(os.listdir(folder)):
             # For iamges containing a single time-point
             if "t" not in dimensions_map:
                 
-                sd, maxsd = focused_z(img) # Get an array with the sd per slice and the position of the maximal sd
+                sd, maxsd = focused_z(img, dimensions_indices) # Get an array with the sd per slice and the position of the maximal sd
                 
                 # Perform the projection and save
-                projection_3D(img)
+                img_proj= projection_3D(img, dimensions_indices)
+                
+                # Save the projected image
+                img_path = os.path.join(folder,'projections', f"{basename}_{proj_type}.tif")
+                imsave(img_path,img_proj)
             
             # For time-lapse images
             elif "t" in dimensions_map:
@@ -423,10 +425,10 @@ for file in sorted(os.listdir(folder)):
                     s_dimensions_map, s_dimensions_indices =redefine_dimensions('t')
                     
                     # Obtain the sd info per each stack and the index of the best one
-                    sd, maxsd = focused_z(img_tslice) # Get an array with the sd per slice and the position of the maximal sd
+                    sd, maxsd = focused_z(img_tslice, s_dimensions_indices) # Get an array with the sd per slice and the position of the maximal sd
                     
                     # Perform the projection and return the projected image
-                    img_proj_tp = projection_4D(img_tslice,'projections')
+                    img_proj_tp = projection_3D(img_tslice, s_dimensions_indices)
                     
                     # Concatenate slice to time-lapse array
                     # Append the projected slice to the list
@@ -438,25 +440,6 @@ for file in sorted(os.listdir(folder)):
                 # Save the projected image
                 img_path = os.path.join(folder,'projections', f"{basename}_{proj_type}.tif")
                 imsave(img_path,img_stack)        
-                    
-                # things to do add metadata to stored image 
-                # properly write all the functions
-                # clone to repository
         
         else:
             raise ValueError(f"Unsupported image dimensions. {filename} does not contain z-stacks")
-
-            
-            
-            
-        
-        
-    
-
-
-
-
-
-
-
-
